dc4fe4522a0c3a28418756ec8143aa31bb153a43
danielcuraj <danielcuraj08@gmail.com>
2026-05-07 08:27:30 +0200

    Modify giocatore and partita table definitions
    Updated table structure to include AUTO_INCREMENT for primary keys and added foreign key constraints.

--- tabelle.sql
+++ tabelle.sql
@@ -1,11 +1,18 @@
 CREATE TABLE giocatore(
-id_giocatore VARCHAR(50) PRIMARY KEY NOT NULL,
+id_giocatore AUTO_INCREMENT PRIMARY KEY NOT NULL,
 cognome VARCHAR(50) NOT NULL,
 nome VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE partita(
-id_giocatore VARCHAR(50) PRIMARY KEY NOT NULL,
+id_partita INT AUTO_INCREMENT NOT NULL,
+id_giocatoreX INT NOT NULL,
+id_giocatoreO INT NOT NULL,
+data DATE PRIMARY KEY,
+FOREIGN KEY (id_giocatoreX) REFERENCES giocatore(id_giocatore)
+FOREIGN KEY (id_giocatoreO) REFERENCES giocatore(id_giocatore)
+
+
   
 
 
