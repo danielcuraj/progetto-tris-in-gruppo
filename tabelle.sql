2da223064660335b0e9f551a429b2ad4cfd8ca17
danielcuraj <danielcuraj08@gmail.com>
2026-05-14 08:33:37 +0200

    Add data_partita column to partita table

--- tabelle.sql
+++ tabelle.sql
@@ -7,6 +7,7 @@ CREATE TABLE partita(
 id_partita INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
 id_giocatoreX INT NOT NULL,
 id_giocatoreO INT NOT NULL,
+data_partita DATE NOT NULL,
 vincitore VARCHAR(50) NOT NULL,
 FOREIGN KEY (id_giocatoreX) REFERENCES giocatore(id_giocatore),
 FOREIGN KEY (id_giocatoreO) REFERENCES giocatore(id_giocatore)
